1eeb8129e65f9515724904b310a218b5540097e3
dat-adi <dat.adithya@gmail.com>
2021-03-11 19:26:00 +0530

    Added .xml files

---
+++ ETH/XMLTest/xml-access.js
@@ -0,0 +1,20 @@
+function loadDoc(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            xmlFunc(this);
+        }
+    };
+    xhttp.open("GET", "http://127.0.0.1/XMLTest/info.xml", true);
+    xhttp.send();
+}
+function xmlFunc(xml){
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Subject Code</th><th>Subject Name</th></tr>";
+    var x = xmlDoc.getElementsByTagName("sub");
+    for(i = 0; i < x.length; i++){
+        table += "<tr><td>" + x[i].getElementsByTagName("code")[0].childNodes[0].nodeValue + "</td><td>" + x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + "</td></tr>";
+    }
+    document.getElementById("info").innerHTML = table;
+}
